ce41f71c3d3c2ca57ffece798a0767ae13b02632
Dirk Grunwald <dirk.grunwald@gmail.com>
2020-10-06 00:09:54 +0000

    update remainder, README

--- 06-RPC/rpc-server.py
+++ 06-RPC/rpc-server.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 import pika
 
-import os
-hostname= os.environ['RABBIT_HOST'] if 'RABBIT_HOST' in os.environ else 'localhost'
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=hostname))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 
 channel = connection.channel()
 
 channel.queue_declare(queue='rpc_queue')
+
 
 def fib(n):
     if n == 0:
@@ -17,23 +15,25 @@ def fib(n):
     elif n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2)
+
 
 def on_request(ch, method, props, body):
     n = int(body)
 
-    print " [.] fib(%s)"  % (n,)
+    print(" [.] fib(%s)" % n)
     response = fib(n)
 
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
-                                                     props.correlation_id),
+                                                         props.correlation_id),
                      body=str(response))
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
+channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
 
-print " [x] Awaiting RPC requests"
+print(" [x] Awaiting RPC requests")
 channel.start_consuming()
--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 Tutorials for RabbitMQ
 
 These are adapted from [the official RabbitMQ tutorials](https://www.rabbitmq.com/getstarted.html).
+
+Assuming you have Docker install (*e.g.* in Google Cloud Console), you can start a rabbitmq server by running
+
+```
+docker run -d --hostname my-rabbit --name some-rabbit -p 5672:5672 rabbitmq:3
+```
+This will expose the RabbitMQ client TCP port 5672 to the local machine.
+
+You will probably need to install the [`pika`](https://pika.readthedocs.io/en/stable/) library to run the examples.
+You can do this using
+```
+pip3 install pika
+```
+
+The sample applications can then be executed.
